715027eee02c6527ea2c2329bf4199a44f6808c7
tayzhang <tayzhang@tencent.com>
2022-10-01 22:48:38 +0800

    move example to posts
    Signed-off-by: tayzhang <tayzhang@tencent.com>

--- user/config/QWER.config.js
+++ user/config/QWER.config.js
@@ -49,7 +49,7 @@ export const CoreConfig = {
   StaticFolder: 'static',
   BuildFolder: 'build',
   UserPublicFolder: 'user/public',
-  UserBlogsFolder: 'user/blogs',
+  UserBlogsFolder: 'docs/_posts',
   UserAssetsFolder: 'user/assets',
   RouteFolder: 'src/routes',
   TemplateFolder: 'src/lib/template',
